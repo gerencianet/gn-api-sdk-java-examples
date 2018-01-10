5f9baa5f4496fca43a3fe28c1ee214c891c70ae0
Filipe Santos <filipe.santos@gerencianet.com.br>
2018-01-10 10:54:14 -0200

    Add new examples

---
+++ src/main/java/br/com/gerencianet/subscriptions/map/CreteSubscriptionHistory.java
@@ -0,0 +1,41 @@
+package br.com.gerencianet.charge.map;
+
+import java.util.HashMap;
+import java.util.Map;
+import br.com.gerencianet.Credentials;
+import br.com.gerencianet.gnsdk.Gerencianet;
+import br.com.gerencianet.gnsdk.exceptions.GerencianetException;
+
+public class CreateChargeHistory {
+	public static void main(String[] args) {
+		/* *********  Set credential parameters ******** */
+
+		Credentials credentials = new Credentials();
+
+		HashMap<String, Object> options = new HashMap<String, Object>();
+		options.put("client_id", credentials.getClientId());
+		options.put("client_secret", credentials.getClientSecret());
+		options.put("sandbox", credentials.isSandbox());
+
+		/* ************************************************* */ 
+
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("id", "0");
+		
+		Map<String, Object> body = new HashMap<String, Object>();
+		body.put("description", "This subscription was not fully paid");
+
+		try {
+			Gerencianet gn = new Gerencianet(options);
+			Map<String, Object> response = gn.call("createSubscriptionHistory", params, body);
+			System.out.println(response);
+		}catch (GerencianetException e){
+			System.out.println(e.getCode());
+			System.out.println(e.getError());
+			System.out.println(e.getErrorDescription());
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
