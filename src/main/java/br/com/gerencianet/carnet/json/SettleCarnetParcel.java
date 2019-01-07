5e8aa8e62b540b7dc9e11bbdc86338f966520826
Thiago Resende <thiagogresende@gmail.com>
2019-01-07 08:35:23 -0200

    Add new endpoints (SettleCharge and SettleCarnetParcel)

---
+++ src/main/java/br/com/gerencianet/carnet/json/SettleCarnetParcel.java
@@ -0,0 +1,44 @@
+package br.com.gerencianet.carnet.json;
+
+import java.util.HashMap;
+
+import org.json.JSONObject;
+
+import br.com.gerencianet.Credentials;
+import br.com.gerencianet.gnsdk.Gerencianet;
+import br.com.gerencianet.gnsdk.exceptions.GerencianetException;
+
+public class SettleCarnetParcel {
+
+	public static void main(String[] args) {
+		/* *********  Set credentials parameters ******** */
+
+		Credentials credentials = new Credentials();
+
+		JSONObject options = new JSONObject();
+		options.put("client_id", credentials.getClientId());
+		options.put("client_secret", credentials.getClientSecret());
+		options.put("sandbox", credentials.isSandbox());
+
+		/* ************************************************* */ 
+
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("id", "0");
+		params.put("parcel", "1");
+
+		try {
+			Gerencianet gn = new Gerencianet(options);
+			JSONObject response = gn.call("settleCarnetParcel", params, new JSONObject());
+			System.out.println(response);
+		}catch (GerencianetException e){
+			System.out.println(e.getCode());
+			System.out.println(e.getError());
+			System.out.println(e.getErrorDescription());
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
